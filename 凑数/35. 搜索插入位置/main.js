505af4e9e9f23c2666bc375a307ecf89c8149fa1
海胆 <8065350+seaurchin486@user.noreply.gitee.com>
2023-02-19 11:11:10 +0800

    35. 搜索插入位置 二分法

--- 凑数/35. 搜索插入位置/main.js
+++ 凑数/35. 搜索插入位置/main.js
@@ -8,24 +8,42 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
-    let index = null
-    nums.some((item,idx) => {
-        if (item >= target) {
-            index = idx
-            return true
-        } else {
-            return false
-        }
-    })
-    if (index === null) {
-        return nums.length
+var searchInsert = function (nums, target) {
+  function halfSearch(left, right) {
+    let half = Math.floor((left + right) / 2)
+    if (nums[half] > target) {
+      if (right - left >= 2) {
+        return halfSearch(left, half)
+      } else {
+        return half
+      }
+    } else if (nums[half] === target) {
+      return half
+    } else if (nums[half] < target) {
+      if (right - left >= 2) {
+        return halfSearch(half, right)
+      } else {
+        return half + 1
+      }
     }
-    return index
+  }
+  if (nums.at(-1) < target) {
+    return nums.length
+  }
+  if (nums[0] > target) {
+    return 0
+  }
+  return halfSearch(0, nums.length-1)
 };
 
 /*
 忽略了查询完整个数组都不存在的情况
 null 和 0 的布尔值都是false。。不能偷懒啊
 woc，忘了查找还有相关算法
+犯了赋值当等于的低级错误，又忘记数组长度有可能为1了
+如果一开始不在区间内，二分就没有意义了
 */
+
+let nums = [1, 3, 5, 6]
+let target = 2
+let output = searchInsert(nums, target)
